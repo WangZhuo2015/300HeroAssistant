a197d7109953f04d4e23dfc944cd19a846f710a0
王卓 <280034816@qq.com>
2016-05-14 01:13:17 +0800

    开黑分析开始开发

---
+++ Tk300HeroAssistant/Model/FriendAnyalysis/FriendAnalyzer.swift
@@ -0,0 +1,13 @@
+//
+//  FriendAnalyzer.swift
+//  Tk300HeroAssistant
+//
+//  Created by 王卓 on 16/5/14.
+//  Copyright © 2016年 SherryTeam. All rights reserved.
+//
+
+import Foundation
+class FriendAnalyzer {
+    
+    
+}
