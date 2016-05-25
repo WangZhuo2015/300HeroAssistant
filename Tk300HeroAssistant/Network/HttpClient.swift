dffef68119d0eb822a71b890834f716b054ffa7d
王卓 <280034816@qq.com>
2016-05-25 16:27:21 +0800

    彻底移除EVReflection

--- Tk300HeroAssistant/Network/HttpClient.swift
+++ Tk300HeroAssistant/Network/HttpClient.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 import Alamofire
-//import EVReflection
-//import AlamofireJsonToObjects
 class HttpClient{
     class func invoke (
         url:String,
@@ -21,31 +19,4 @@ class HttpClient{
             }
         }
     }
-    
-    
-    
-//    class func invoke (
-//        url:String,
-//        parameters:[String: AnyObject]? = nil,
-//        complete:((response: NSData?, error: NSError?) -> Void)?){
-//        Alamofire.request(.GET, url, parameters: parameters, encoding: .URL, headers: [:]).validate(statusCode: 200..<300).responseJSON { (response:Response<AnyObject, NSError>) -> Void in
-//            print(response.result.value)
-//            if let handle = complete{
-//                handle(response: response.data, error: response.result.error)
-//            }
-//        }
-//    }
-//    
-//    class func invokeObject< T: EVObject >(
-//        url:String,
-//        parameters:[String: AnyObject]? = nil,
-//        complete:(object: T?, error: NSError?) -> Void){
-//        print(url)
-//        print(parameters)
-//
-//        Alamofire.request(.GET, url, parameters: nil, encoding: .URL, headers: [:]).validate(statusCode: 200..<300).responseObject { (URLRequest:NSURLRequest?, HTTPURLResponse:NSHTTPURLResponse?, result:Result< T, NSError>) in
-//            //print(result.value)
-//            complete(object: result.value, error: result.error)
-//            }
-//    }
 }
