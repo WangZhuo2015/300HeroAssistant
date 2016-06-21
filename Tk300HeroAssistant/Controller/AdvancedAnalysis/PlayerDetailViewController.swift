d0423d6792e482072feb16c7467eecc3e78fb0de
王卓 <280034816@qq.com>
2016-06-22 02:11:46 +0800

    开黑玩家详情init

---
+++ Tk300HeroAssistant/Controller/AdvancedAnalysis/PlayerDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlayerDetailViewController.swift
+//  Tk300HeroAssistant
+//
+//  Created by 王卓 on 16/6/22.
+//  Copyright © 2016年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var headerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
