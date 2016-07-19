b5d1ac557d39294e0c8137dc77a3c7d023a15ead
王卓 <280034816@qq.com>
2016-07-19 13:14:01 +0800

    加点界面init

---
+++ Tk300HeroAssistant/Cells/HeroPointAddTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeroPointAddTableViewCell.swift
+//  Tk300HeroAssistant
+//
+//  Created by 王卓 on 16/7/18.
+//  Copyright © 2016年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+
+class HeroPointAddTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
